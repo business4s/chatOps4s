810f7d8df7a1d464285afa93324e89c1c52c636b
Liam Grossman <liam.j.grossman@gmail.com>
2025-06-24 15:56:14 -0400

    create a registry
    A context for interactions (messages and their respective senders).

---
+++ chatops4s-discord/src/main/scala/api/DiscordRegistry.scala
@@ -0,0 +1,30 @@
+package api
+
+import cats.effect.IO
+import models.InteractionContext
+import models.Message
+import models.MessageResponse
+
+import scala.collection.concurrent.TrieMap
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+
+class DiscordRegistry {
+  val interactions: TrieMap[String, (Message, (String, Message) => IO[MessageResponse])] = TrieMap.empty
+
+  def registerInteraction(message: Message, sender: (String, Message) => IO[MessageResponse]): String = {
+    val id = java.util.UUID.randomUUID().toString.take(n = 8)
+    interactions += id -> (message, sender)
+    id
+  }
+
+  def removeInteraction(id: String): Boolean = {
+    val maybeInteraction = interactions.get(id)
+    maybeInteraction match {
+      case Some(interaction) =>
+        interactions.remove(id)
+        true
+      case None => false
+    }
+  }
+}
