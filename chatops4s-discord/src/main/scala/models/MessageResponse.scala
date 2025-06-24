7f5a4d4f3cea525cd5e8f7c61c4ae1fd43cd3026
Liam Grossman <liam.j.grossman@gmail.com>
2025-06-24 16:46:44 -0400

    Update MessageResponse.scala

--- chatops4s-discord/src/main/scala/models/MessageResponse.scala
+++ chatops4s-discord/src/main/scala/models/MessageResponse.scala
@@ -1,12 +1,6 @@
 package models
 
-import io.circe.{Decoder, Encoder}
-import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 
 case class MessageResponse(
   messageId: String
 )
-object MessageResponse {
-  implicit val decoder: Decoder[MessageResponse] = deriveDecoder
-  implicit val encoder: Encoder[MessageResponse] = deriveEncoder
-}
