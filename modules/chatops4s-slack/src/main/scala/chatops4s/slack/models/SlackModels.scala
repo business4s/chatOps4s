f21e1f30723156f13cc04da7eb86b83b68719172
masterhj <jaiswalhiman@gmail.com>
2025-07-16 11:06:30 +0530

    using derive codecs!!

--- modules/chatops4s-slack/src/main/scala/chatops4s/slack/models/SlackModels.scala
+++ modules/chatops4s-slack/src/main/scala/chatops4s/slack/models/SlackModels.scala
@@ -1,9 +1,7 @@
 package chatops4s.slack.models
 
-import io.circe.{Decoder, Encoder}
-import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import io.circe.{Codec, Decoder, Encoder}
 import pureconfig.ConfigReader
-
 
 case class SlackConfig(
                         botToken: String,
@@ -16,19 +14,19 @@ case class SlackPostMessageRequest(
                                     text: String,
                                     blocks: Option[List[SlackBlock]] = None,
                                     thread_ts: Option[String] = None
-                                  )
+                                  ) derives Codec.AsObject
 
 case class SlackBlock(
                        `type`: String,
                        text: Option[SlackText] = None,
                        elements: Option[List[SlackBlockElement]] = None,
                        accessory: Option[SlackBlockElement] = None
-                     )
+                     ) derives Codec.AsObject
 
 case class SlackText(
                       `type`: String,
                       text: String
-                    )
+                    ) derives Codec.AsObject
 
 case class SlackBlockElement(
                               `type`: String,
@@ -36,21 +34,23 @@ case class SlackBlockElement(
                               action_id: Option[String] = None,
                               value: Option[String] = None,
                               style: Option[String] = None
-                            )
+                            ) derives Codec.AsObject
+
 case class SlackPostMessageResponse(
                                      ok: Boolean,
                                      channel: Option[String] = None,
                                      ts: Option[String] = None,
                                      message: Option[SlackMessage] = None,
                                      error: Option[String] = None
-                                   )
+                                   ) derives Codec.AsObject
 
 case class SlackMessage(
                          text: String,
                          user: Option[String] = None,
                          ts: String,
                          thread_ts: Option[String] = None
-                       )
+                       ) derives Codec.AsObject
+
 case class SlackInteractionPayload(
                                     `type`: String,
                                     user: SlackUser,
@@ -61,27 +61,27 @@ case class SlackInteractionPayload(
                                     message: Option[SlackMessage] = None,
                                     actions: Option[List[SlackAction]] = None,
                                     response_url: Option[String] = None
-                                  )
+                                  ) derives Codec.AsObject
 
 case class SlackUser(
                       id: String,
                       name: String
-                    )
+                    ) derives Codec.AsObject
 
 case class SlackTeam(
                       id: String,
                       domain: String
-                    )
+                    ) derives Codec.AsObject
 
 case class SlackChannel(
                          id: String,
                          name: String
-                       )
+                       ) derives Codec.AsObject
 
 case class SlackContainer(
                            `type`: String,
                            message_ts: Option[String] = None
-                         )
+                         ) derives Codec.AsObject
 
 case class SlackAction(
                         action_id: String,
@@ -90,42 +90,4 @@ case class SlackAction(
                         value: Option[String] = None,
                         `type`: String,
                         action_ts: String
-                      )
-
-object SlackModels {
-  implicit val slackTextEncoder: Encoder[SlackText] = deriveEncoder
-  implicit val slackTextDecoder: Decoder[SlackText] = deriveDecoder
-
-  implicit val slackBlockElementEncoder: Encoder[SlackBlockElement] = deriveEncoder
-  implicit val slackBlockElementDecoder: Decoder[SlackBlockElement] = deriveDecoder
-
-  implicit val slackBlockEncoder: Encoder[SlackBlock] = deriveEncoder
-  implicit val slackBlockDecoder: Decoder[SlackBlock] = deriveDecoder
-
-  implicit val slackPostMessageRequestEncoder: Encoder[SlackPostMessageRequest] = deriveEncoder
-  implicit val slackPostMessageRequestDecoder: Decoder[SlackPostMessageRequest] = deriveDecoder
-
-  implicit val slackMessageEncoder: Encoder[SlackMessage] = deriveEncoder
-  implicit val slackMessageDecoder: Decoder[SlackMessage] = deriveDecoder
-
-  implicit val slackPostMessageResponseEncoder: Encoder[SlackPostMessageResponse] = deriveEncoder
-  implicit val slackPostMessageResponseDecoder: Decoder[SlackPostMessageResponse] = deriveDecoder
-
-  implicit val slackUserEncoder: Encoder[SlackUser] = deriveEncoder
-  implicit val slackUserDecoder: Decoder[SlackUser] = deriveDecoder
-
-  implicit val slackTeamEncoder: Encoder[SlackTeam] = deriveEncoder
-  implicit val slackTeamDecoder: Decoder[SlackTeam] = deriveDecoder
-
-  implicit val slackChannelEncoder: Encoder[SlackChannel] = deriveEncoder
-  implicit val slackChannelDecoder: Decoder[SlackChannel] = deriveDecoder
-
-  implicit val slackContainerEncoder: Encoder[SlackContainer] = deriveEncoder
-  implicit val slackContainerDecoder: Decoder[SlackContainer] = deriveDecoder
-
-  implicit val slackActionEncoder: Encoder[SlackAction] = deriveEncoder
-  implicit val slackActionDecoder: Decoder[SlackAction] = deriveDecoder
-
-  implicit val slackInteractionPayloadEncoder: Encoder[SlackInteractionPayload] = deriveEncoder
-  implicit val slackInteractionPayloadDecoder: Decoder[SlackInteractionPayload] = deriveDecoder
-}
+                      ) derives Codec.AsObject
