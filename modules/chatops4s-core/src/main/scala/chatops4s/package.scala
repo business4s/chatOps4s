b54895a3c6eab4b46da501be5f941d752230d16b
masterhj <jaiswalhiman@gmail.com>
2025-07-23 17:16:59 +0530

    codec format

--- modules/chatops4s-core/src/main/scala/chatops4s/package.scala
+++ modules/chatops4s-core/src/main/scala/chatops4s/package.scala
@@ -1,26 +1,27 @@
 package chatops4s
 
 import cats.effect.IO
+import io.circe.Codec.AsObject
 
 case class Message(
   text: String,
   interactions: Seq[Button] = Seq.empty
-)
+) derives AsObject
 
 case class MessageResponse(
   messageId: String
-)
+) derives AsObject
 
 case class Button(
   label: String,
   value: String
-)
+) derives AsObject
 
 case class InteractionContext(
   userId: String,
   channelId: String,
   messageId: String
-)
+) derives AsObject
 
 trait OutboundGateway {
   def sendToChannel(channelId: String, message: Message): IO[MessageResponse]
