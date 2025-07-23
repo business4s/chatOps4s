f8389eaec042d112ccaf1b2da2d81ecf16ca5d12
masterhj <jaiswalhiman@gmail.com>
2025-07-23 14:46:02 +0530

    sbtfmtALl format

--- modules/chatops4s-core/src/main/scala/chatops4s/package.scala
+++ modules/chatops4s-core/src/main/scala/chatops4s/package.scala
@@ -2,25 +2,26 @@ package chatops4s
 
 import cats.effect.IO
 
-// Core domain models
 case class Message(
-                    text: String,
-                    interactions: Seq[Button] = Seq.empty,
-                  )
+  text: String,
+  interactions: Seq[Button] = Seq.empty
+)
 
 case class MessageResponse(
-                            messageId: String,
-                          )
+  messageId: String
+)
 
-case class Button(label: String, value: String)
+case class Button(
+  label: String,
+  value: String
+)
 
 case class InteractionContext(
-                               userId: String,
-                               channelId: String,
-                               messageId: String,
-                             )
+  userId: String,
+  channelId: String,
+  messageId: String
+)
 
-// Core gateways
 trait OutboundGateway {
   def sendToChannel(channelId: String, message: Message): IO[MessageResponse]
   def sendToThread(messageId: String, message: Message): IO[MessageResponse]
