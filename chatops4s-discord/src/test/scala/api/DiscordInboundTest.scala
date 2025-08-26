efec4343e37305868ca9497a5da92e6449e1be1d
Liam Grossman <liam.j.grossman@gmail.com>
2025-08-25 21:22:25 -0400

    Update DiscordInboundTest.scala

--- chatops4s-discord/src/test/scala/api/DiscordInboundTest.scala
+++ chatops4s-discord/src/test/scala/api/DiscordInboundTest.scala
@@ -1,37 +1,40 @@
 package api
 
 import cats.effect.IO
-import models.{Button, InteractionContext}
-import org.scalatest.funsuite.AnyFunSuite
+import models.InteractionContext
 import org.scalatest.matchers.should.Matchers
 import cats.effect.unsafe.implicits.global
+import org.scalatest.freespec.AnyFreeSpec
 
-class DiscordInboundTest extends AnyFunSuite with Matchers {
-  test("registerAction should store handler and return a button factory") {
-    val discordInbound = new DiscordInbound
+class DiscordInboundTest extends AnyFreeSpec with Matchers {
+  "DiscordInbound" - {
+    "registerAction should store handler and return a button factory" in {
+      val discordInbound = new DiscordInbound
 
-    var executed = false
-    val handler: InteractionContext => IO[Unit] = _ => IO { executed = true }
+      var executed = false
+      val handler: InteractionContext => IO[Unit] = _ => IO {
+        executed = true
+      }
 
-    val registeredAction = discordInbound.registerAction(handler)
-    val button = registeredAction.render("Click me")
+      val registeredAction = discordInbound.registerAction(handler)
+      val button = registeredAction.render("Click me")
 
-    button.label shouldBe "Click me"
-    button.value should not be empty
+      button.label shouldBe "Click me"
+      button.value should not be empty
 
-    discordInbound.handlers.get(button.value) should not be empty
+      discordInbound.handlers.get(button.value) should not be empty
 
-    discordInbound.handlers(button.value)(InteractionContext("user", "channelId", "messageId")).unsafeRunSync()
-    executed shouldBe true
-  }
+      discordInbound.handlers(button.value)(InteractionContext("user", "channelId", "messageId")).unsafeRunSync()
+      executed shouldBe true
+    }
+    "each registered action should have a unique id" in {
+      val discordInbound = new DiscordInbound
+      val handler: InteractionContext => IO[Unit] = _ => IO {}
 
-  test("each registered action should have a unique id") {
-    val discordInbound = new DiscordInbound
-    val handler: InteractionContext => IO[Unit] = _ => IO { }
+      val registeredAction1 = discordInbound.registerAction(handler).render("")
+      val registeredAction2 = discordInbound.registerAction(handler).render("")
 
-    val registeredAction1 = discordInbound.registerAction(handler).render("")
-    val registeredAction2 = discordInbound.registerAction(handler).render("")
-
-    registeredAction1.value should not equal registeredAction2.value
+      registeredAction1.value should not equal registeredAction2.value
+    }
   }
 }
